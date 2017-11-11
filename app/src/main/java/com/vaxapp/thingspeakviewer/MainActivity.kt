036fd337d38e52b94e33364dd49837eebf6a8db0
valeria <valerie.cardoso@gmail.com>
2017-11-11 20:17:11 +0100

    added loading function

--- app/src/main/java/com/vaxapp/thingspeakviewer/MainActivity.kt
+++ app/src/main/java/com/vaxapp/thingspeakviewer/MainActivity.kt
@@ -2,8 +2,19 @@ package com.vaxapp.thingspeakviewer
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.vaxapp.thingspeakviewer.data.ApiService
+import com.vaxapp.thingspeakviewer.data.Channel
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
+
+    val service by lazy {
+        ApiService.create()
+    }
+    var disposable: Disposable? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,7 +23,23 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun loadData() {
+        disposable =
+                service.fetchFields("https://api.thingspeak.com/channels/298096/feeds.json?results=1")
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(
+                                { result -> showResult(result.channel) },
+                                { _ -> showError() }
+                        )
+    }
 
+    private fun showError() {
+        //make more Kotlin
+        Toast.makeText(this, "Error loading channel data", Toast.LENGTH_LONG).show();
+    }
 
+    private fun showResult(channel: Channel) {
+
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }

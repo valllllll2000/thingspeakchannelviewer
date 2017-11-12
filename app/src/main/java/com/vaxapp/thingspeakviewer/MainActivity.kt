996c4bc7083151016b65859fd38622a6016cb789
valeria <valerie.cardoso@gmail.com>
2017-11-12 13:54:10 +0100

    added menu to access settings

--- app/src/main/java/com/vaxapp/thingspeakviewer/MainActivity.kt
+++ app/src/main/java/com/vaxapp/thingspeakviewer/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.vaxapp.thingspeakviewer
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        getMenuInflater().inflate(R.menu.main, menu)
+        return true;
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        if (id == R.id.settings) {
+            startActivity(Intent(this, SettingsActivity::class.java))
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }

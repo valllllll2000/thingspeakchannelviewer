fa4a51baffdf3cc9551950b13977438beee6c88d
valeria <valerie.cardoso@gmail.com>
2017-11-12 13:48:39 +0100

    removed useless comment

--- app/src/main/java/com/vaxapp/thingspeakviewer/SettingsActivity.kt
+++ app/src/main/java/com/vaxapp/thingspeakviewer/SettingsActivity.kt
@@ -17,16 +17,6 @@ import android.preference.RingtonePreference
 import android.text.TextUtils
 import android.view.MenuItem
 
-/**
- * A [PreferenceActivity] that presents a set of application settings. On
- * handset devices, settings are presented as a single list. On tablets,
- * settings are split by category, with category headers shown to the left of
- * the list of settings.
- *
- * See [Android Design: Settings](http://developer.android.com/design/patterns/settings.html)
- * for design guidelines and the [Settings API Guide](http://developer.android.com/guide/topics/ui/settings.html)
- * for more information on developing a Settings UI.
- */
 class SettingsActivity : AppCompatPreferenceActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {

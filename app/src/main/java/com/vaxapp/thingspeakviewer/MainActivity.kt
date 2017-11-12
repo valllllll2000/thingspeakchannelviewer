cefcaa137305c27a7a756ca04ed0bd6cd0a7b39d
valeria <valerie.cardoso@gmail.com>
2017-11-12 17:35:36 +0100

    added data display

--- app/src/main/java/com/vaxapp/thingspeakviewer/MainActivity.kt
+++ app/src/main/java/com/vaxapp/thingspeakviewer/MainActivity.kt
@@ -10,11 +10,18 @@ import com.vaxapp.thingspeakviewer.data.ApiService
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
+import org.jetbrains.anko.error
 import org.jetbrains.anko.toast
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity(), AnkoLogger {
+
+    private val url = "https://api.thingspeak.com/channels/298096/feeds.json?results=1"
 
     val service by lazy {
         ApiService.create()
@@ -25,32 +32,49 @@ class MainActivity : AppCompatActivity(), AnkoLogger {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
         loadData();
     }
 
     private fun loadData() {
         disposable =
-                service.fetchFields("https://api.thingspeak.com/channels/298096/feeds.json?results=1")
+                service.fetchFields(url)
                         .subscribeOn(Schedulers.io())
                         .observeOn(AndroidSchedulers.mainThread())
                         .subscribe(
                                 { result -> showResult(result) },
-                                { _ -> showError() }
+                                { error -> showError(error) }
                         )
     }
 
-    private fun showError() {
-        //make more Kotlin
-       toast("Error loading channel data2");
+    private fun showError(error: Throwable) {
+        toast("Error loading channel data")
+        error(error)
     }
 
     private fun showResult(response: ApiResponse) {
         info("channel " + response)
+        descriptionTv.text = response.channel.description;
+        field1Label.text = response.channel.field1
+        field2Label.text = response.channel.field2
+        field1Content.text = response.feeds[0].field1
+        field2Content.text = response.feeds[0].field2
+        updatedTv.text = getString(R.string.last_updated_at, getFormattedDate(response))
     }
+
+    private fun getFormattedDate(response: ApiResponse): String? {
+        val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
+        simpleDateFormat.setTimeZone(TimeZone.getTimeZone("UTC"))
+        val date = simpleDateFormat.parse(response.channel.updated_at)
+        val localDateFormat = SimpleDateFormat("dd-MM-yyyy HH:mm:ss")
+        localDateFormat.timeZone = TimeZone.getDefault()
+        return localDateFormat.format(date)
+    }
+
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.main, menu)
-        return true;
+        return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -64,6 +88,6 @@ class MainActivity : AppCompatActivity(), AnkoLogger {
 
     override fun onDestroy() {
         super.onDestroy()
-        disposable?.dispose();
+        disposable?.dispose()
     }
 }
--- app/src/main/java/com/vaxapp/thingspeakviewer/SettingsActivity.kt
+++ app/src/main/java/com/vaxapp/thingspeakviewer/SettingsActivity.kt
@@ -1,10 +1,8 @@
 package com.vaxapp.thingspeakviewer
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-
-import kotlinx.android.synthetic.main.activity_settings.*
+import kotlinx.android.synthetic.main.app_bar.*
 
 class SettingsActivity : AppCompatActivity() {
 
@@ -12,11 +10,5 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
         setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
     }
-
 }

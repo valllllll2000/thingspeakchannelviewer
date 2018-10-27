625cc2ef98b8f50b3b6382330d84c90ec8483a90
valeria <valerie.cardoso@gmail.com>
2018-10-27 14:33:04 +0200

    using cache for responses

--- app/src/main/java/com/vaxapp/thingspeakviewer/data/ApiService.kt
+++ app/src/main/java/com/vaxapp/thingspeakviewer/data/ApiService.kt
@@ -3,8 +3,11 @@ package com.vaxapp.thingspeakviewer.data
 import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkInfo
+import android.util.Log
 import com.vaxapp.thingspeakviewer.BuildConfig
 import io.reactivex.Single
+import okhttp3.Cache
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.logging.HttpLoggingInterceptor
@@ -21,37 +24,52 @@ interface ApiService {
 
     companion object {
 
-        fun create(): ApiService {
+        private val REWRITE_RESPONSE_INTERCEPTOR = Interceptor { chain ->
+            val originalResponse = chain.proceed(chain.request())
+            val cacheControl = originalResponse.header("Cache-Control")
+            if (cacheControl == null || cacheControl.contains("no-store") || cacheControl.contains("no-cache") ||
+                    cacheControl.contains("must-revalidate") || cacheControl.contains("max-age=0")) {
+                originalResponse.newBuilder()
+                        .removeHeader("Pragma")
+                        .header("Cache-Control", "public, max-age=" + 5000)
+                        .build()
+            } else {
+                originalResponse
+            }
+        }
+
+
+        fun create(context: Context): ApiService {
 
             val interceptor = HttpLoggingInterceptor()
             if (BuildConfig.DEBUG) {
                 interceptor.level = HttpLoggingInterceptor.Level.BODY
             }
-            val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
-           // val cacheSize = (5 * 1024 * 1024).toLong()
-            //val myCache = Cache(context.cacheDir, cacheSize)
-//            val client = OkHttpClient.Builder()
-//                    .addInterceptor(interceptor)
-//                    // Specify the cache we created earlier.
-//                    //.cache(myCache)
-//                    // Add an Interceptor to the OkHttpClient.
-//                    .addInterceptor { chain ->
+            //val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
+            val cacheSize = (5 * 1024 * 1024).toLong()
+            val myCache = Cache(context.cacheDir, cacheSize)
+            val client = OkHttpClient.Builder()
+                    .addInterceptor(interceptor)
+                    // Specify the cache we created earlier.
+                    .cache(myCache)
+                    // Add an Interceptor to the OkHttpClient.
+                    .addInterceptor { chain ->
 
                         /*
                         *  Leveraging the advantage of using Kotlin,
                         *  we initialize the request and change its header depending on whether
                         *  the device is connected to Internet or not.
                         */
-//                        val request = when {
-//                            hasNetwork(context) -> createRecentCacheRequest(chain.request())
-//                            else ->
-//                                createOlderCacheRequest(chain.request())
-//                        }
+                        val request = when {
+                            hasNetwork(context) -> createRecentCacheRequest(chain.request())
+                            else -> createOlderCacheRequest(chain.request())
+                        }
 
                         // Add the modified request to the chain.
-//                        chain.proceed(createRecentCacheRequest(chain.request()))
-//                    }
-//                    .build()
+                        chain.proceed(request)
+                    }
+                    .addNetworkInterceptor(REWRITE_RESPONSE_INTERCEPTOR)
+                    .build()
 
             val retrofit = Retrofit.Builder()
                     .baseUrl("https://api.thingspeak.com/channels/298096/")
@@ -72,17 +90,27 @@ interface ApiService {
          *  The 'max-stale' attribute is responsible for this behavior.
          *  The 'only-if-cached' attribute indicates to not retrieve new data; fetch the cache only instead.
          */
-        private fun createOlderCacheRequest(request: Request) =
-                request.newBuilder().header("Cache-Control", "public, only-if-cached, max-stale=" + 60 * 60 * 24 * 7).build()
+        private fun createOlderCacheRequest(request: Request) : Request {
+            Log.d("ApiService", "createOlderCacheRequest")
+            return request.newBuilder()
+                    .removeHeader("Pragma")
+                    .header("Cache-Control", "public, only-if-cached, max-stale=" + 60 * 60 * 24 * 7)
+                    .build()
+        }
 
         /**
          * If there is Internet, get the cache that was stored 5 seconds ago.
          *  If the cache is older than 5 seconds, then discard it,
-                       *  and indicate an error in fetching the response.
-                       *  The 'max-age' attribute is responsible for this behavior.
-                       */
-        private fun createRecentCacheRequest(request: Request) =
-                request.newBuilder().header("Cache-Control", "public, max-age=" + 5).build()
+         *  and indicate an error in fetching the response.
+         *  The 'max-age' attribute is responsible for this behavior.
+         */
+        private fun createRecentCacheRequest(request: Request): Request {
+            Log.d("ApiService", "createRecentCacheRequest")
+            return request.newBuilder()
+                    .removeHeader("Pragma")
+                    .header("Cache-Control", "public, max-age=" + 5)
+                    .build()
+        }
 
         private fun hasNetwork(context: Context): Boolean {
             var isConnected = false
@@ -94,5 +122,7 @@ interface ApiService {
             return isConnected
         }
     }
+
+
 
 }
--- app/src/main/java/com/vaxapp/thingspeakviewer/di/DataModule.kt
+++ app/src/main/java/com/vaxapp/thingspeakviewer/di/DataModule.kt
@@ -1,20 +1,22 @@
 package com.vaxapp.thingspeakviewer.di
 
+import android.content.Context
 import com.vaxapp.thingspeakviewer.data.ApiService
 import com.vaxapp.thingspeakviewer.data.DataOfficeWeatherRepository
 import com.vaxapp.thingspeakviewer.data.OfficeWeatherCloudDataSource
 import com.vaxapp.thingspeakviewer.domain.OfficeWeatherRepository
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module.applicationContext
 
 
 val dataModule = applicationContext {
     bean { provideOfficeWeatherRepository(get()) }
     bean { provideOfficeWeatherDataSource(get()) }
-    bean { provideApiService() }
+    bean { provideApiService(androidContext()) }
 }
 
-fun provideApiService(): ApiService {
-    return ApiService.create()
+fun provideApiService(context: Context): ApiService {
+    return ApiService.create(context)
 }
 
 fun provideOfficeWeatherDataSource(get: ApiService): OfficeWeatherCloudDataSource {
